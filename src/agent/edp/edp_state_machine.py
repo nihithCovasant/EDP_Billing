f6acd527e93876e66dfb0ea22a689c9021419766
mitra-js <saimitra.jandhyala@covasant.com>
2026-07-07 12:31:54 +0530

    loop handling with delay

--- src/agent/edp/edp_state_machine.py
+++ src/agent/edp/edp_state_machine.py
@@ -1,6 +1,8 @@
 import time
 from venv import logger
 from collections import defaultdict
+import asyncio
+
 EDP_STATES = ["INITIALIZING", "WAITING_FOR_FILE_UPLOAD", "WAITING_FOR_BILL_POSTING_COMPLETION", "WAITING_FOR_RECON_COMPLETION", "WAITING_FOR_CNG_COMPLETION", "WAITING_FOR_GTG", "SUCCEEDED", "FAILED"]
 
 
@@ -54,24 +56,32 @@ def get_segment_state_handler(segment: str, segment_state:str):
     # Similarly, initialize for other segments like DR, SLBM, etc.
 
 async def state_machine_loop() -> None:
-    current_time = time.time()
-    for segment in SEGMENTS:
-        if is_handled(segment):
-            logger.info(f"Segment {segment} is already handled. Skipping.")
-            continue
-        st_time, end_time = get_segment_time_range(segment)
-        if st_time <= current_time <= end_time:
-            logger.info(f"Segment {segment} is in the time range. Processing.")
-            segment_state = get_segment_state(segment)
-            fn = get_segment_state_handler(segment, segment_state)
-            try:
-                fn(segment,segment_state,current_time)
-            except Exception as e:
-                logger.error(f"Error occurred while processing segment {segment} in function {fn.__name__}: {e}")
-        elif current_time > st_time:
-            move_to_state(segment, "FAILED", f"Segment {segment} in state {segment_state} has exceeded its processing time window. Marking as FAILED.",segment,segment_state)
-        else:
-            logger.info(f"Segment {segment} is not in the time range. Skipping.")
+    try:
+        current_time = time.time()
+        for segment in SEGMENTS:
+            if is_handled(segment):
+                logger.info(f"Segment {segment} is already handled. Skipping.")
+                continue
+            st_time, end_time = get_segment_time_range(segment)
+            if st_time <= current_time <= end_time:
+                logger.info(f"Segment {segment} is in the time range. Processing.")
+                segment_state = get_segment_state(segment)
+                fn = get_segment_state_handler(segment, segment_state)
+                try:
+                    fn(segment,segment_state,current_time)
+                except Exception as e:
+                    logger.error(f"Error occurred while processing segment {segment} in function {fn.__name__}: {e}")
+            elif current_time > st_time:
+                move_to_state(segment, "FAILED", f"Segment {segment} in state {segment_state} has exceeded its processing time window. Marking as FAILED.",segment,segment_state)
+            else:
+                logger.info(f"Segment {segment} is not in the time range. Skipping.")
+    except Exception as e:
+        logger.error(f"Error occurred in state_machine_loop: {e}")
     
+    finally:
+        # schedule the next run of the state machine loop after a delay using asyncio
+        asyncio.sleep(60)  # Wait for 60 seconds
+        asyncio.create_task(state_machine_loop())  # Recursively call the loop to continue processing
+
 
 
